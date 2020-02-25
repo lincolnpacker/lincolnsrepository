816390936bfa71b33ef26d45b1bdb0fd37a0a618
Lincoln Packer <lincolnpacker@gmail.com>
2020-02-25 11:58:06 -0700

    bash test

---
+++ Project/StarWars/starwars.js
@@ -0,0 +1,9 @@
+console.log ('Welcome To My Page')
+
+console.log(document.querySelection(.'greeting'))
+
+const greetingDiv = document.querySelector('.greeting')
+
+greetingDiv.textContent = 'I just inserted text into a DOM element using my mad JavaScript SKills!!'
+
+console.log(greetingDiv.textContent)
